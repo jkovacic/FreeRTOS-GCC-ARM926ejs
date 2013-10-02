ba30e744f7df13d9e777330883e6a5e71f5ce79f
Jernej Kovacic <jkovacic@gmail.com>
2013-10-02 10:55:24 +0200

    fixed frequency periodic tasks

--- Demo/main.c
+++ Demo/main.c
@@ -24,7 +24,6 @@ limitations under the License.
 
 
 #include <stddef.h>
-#include <stdint.h>
 
 #include <FreeRTOS.h>
 #include <task.h>
@@ -41,20 +40,20 @@ limitations under the License.
 /* Struct with settings for each task */
 typedef struct _paramStruct
 {
-    char* text;        /* text to be printed by the task */
-    uint32_t delay;    /* delay in miliseconds */
+    char* text;                      /* text to be printed by the task */
+    unsigned portBASE_TYPE delay;    /* delay in miliseconds */
 } paramStruct;
 
 /* Default parameters if no parameter struct is available */
 static const char defaultText[] = "<NO TEXT>\r\n";
-static const uint32_t defaultDelay = 1000;
+static const unsigned portBASE_TYPE defaultDelay = 1000;
 
 
-/* Task function that will be instantiated in multiple tasks */
+/* Task function - may be instantiated in multiple tasks */
 void vTaskFunction( void *pvParameters )
 {
     const char* taskName;
-    uint32_t delay;
+    unsigned portBASE_TYPE delay;
     paramStruct* params = (paramStruct*) pvParameters;
 
     taskName = ( NULL==params || NULL==params->text ? defaultText : params->text );
@@ -77,11 +76,52 @@ void vTaskFunction( void *pvParameters )
     vTaskDelete(NULL);
 }
 
+
+/* Fixed frequency periodic task function - may be instantiated in multiple tasks */
+void vPeriodicTaskFunction(void* pvParameters)
+{
+    const char* taskName;
+    unsigned portBASE_TYPE delay;
+    paramStruct* params = (paramStruct*) pvParameters;
+    portTickType lastWakeTime;
+
+    taskName = ( NULL==params || NULL==params->text ? defaultText : params->text );
+    delay = ( NULL==params ? defaultDelay : params->delay);
+
+    /*
+     * This variable must be initialized once.
+     * Then it will be updated automatically by vTaskDelayUntil().
+     */
+    lastWakeTime = xTaskGetTickCount();
+
+    for( ; ; )
+    {
+        /* Print out the name of this task. */
+
+        vPrintMsg(taskName);
+
+        /*
+         * The task will unblock exactly after 'delay' miliseconds (actually
+         * after the appropriate number of ticks), relative from the moment
+         * it was last unblocked.
+         */
+        vTaskDelayUntil( &lastWakeTime, delay / portTICK_RATE_MS );
+    }
+
+    /*
+     * If the task implementation ever manages to break out of the
+     * infinite loop above, it must be deleted before reaching the
+     * end of the function!
+     */
+    vTaskDelete(NULL);
+}
+
+
 /* Parameters for two tasks */
 static const paramStruct tParam[2] =
 {
     (paramStruct) { .text="Task1\r\n", .delay=2000 },
-    (paramStruct) { .text="Task2\r\n", .delay=3000 }
+    (paramStruct) { .text="Periodic task\r\n", .delay=3000 }
 };
 
 
@@ -139,7 +179,7 @@ void main(void)
         FreeRTOS_Error("Could not create task1\r\n");
     }
 
-    if ( pdPASS != xTaskCreate(vTaskFunction, "task2", 128, (void*) &tParam[1], 2, NULL) )
+    if ( pdPASS != xTaskCreate(vPeriodicTaskFunction, "task2", 128, (void*) &tParam[1], 2, NULL) )
     {
         FreeRTOS_Error("Could not create task2\r\n");
     }
