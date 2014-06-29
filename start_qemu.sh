25bc12c08e93f75d25eba3abdcdbac0141b6b861
Jernej Kovacic <jkovacic@gmail.com>
2014-06-29 21:36:35 +0200

    improved Qemu startup script with a possibility to invoke a GDB server

--- start_qemu.sh
+++ start_qemu.sh
@@ -1,23 +1,51 @@
 #!/bin/bash
 #
-# Usage: start_qemu.sh [image_file]
+# Usage: start_qemu.sh [image_file] [-debug]
 #
 # Runs an instance of "qemu-system-arm", emulating an ARM Versatile Application Baseboard with
 # ARM926EJ-S. Qemu is run in the "nographics mode", i.e. its display will not open and the
 # board's UART0 serial port will be "connected" directly to the running shell's standard output
 # and/or input.
 #
-# If no command line argument is provided, "image.bin" (as built by Makefile) is executed as
+# If no image_file argument is provided, "image.bin" (as built by Makefile) is executed as
 # a firmware image file.
+#
+# If '-debug' is specified, the Qemu will be executed with additional "-S -s" flags.
+# This will cause Qemu to act as a GDB server, listening on the TCP port 1234.
+# Execution of the image will freeze at the startup point.
+#
+# For more details, see:
+# http://winfred-lu.blogspot.com/2011/12/arm-u-boot-on-qemu.html
 
-# Default firmware image:
-IMAGE_FILE=image.bin
+# Default firmware image if no other one is provided:
+DEF_IMAGE_FILE=image.bin
 
-# Assign the first argument to IMAGE_FILE if at least one CLI argument is provided:
-if [ $# -ge 1 ]; then
-    IMAGE_FILE=$1;
+# Qemu arguments to invoke Qemu with a GDB server listening on the 
+# TCP port 1234 and freeze execution of the image at startup.
+QEMU_GDB='-S -s'
+
+# Initial values of both Qemu arguments:
+QEMU_GDB_ARG=''
+IMAGE_FILE=''
+
+# Check all CLI arguments to this script. The first argument, not equal
+# to "-debug", will be considered as the desired image name. All 
+# remaining arguments, not equal to "-debug", will be discarded.
+ 
+for arg in $*; do
+  if [ "$arg" = '-debug' ]; then
+    QEMU_GDB_ARG=$QEMU_GDB;
+  else
+    if [ "$IMAGE_FILE" = '' ]; then
+      IMAGE_FILE=$arg
+    fi
+  fi
+done
+
+# If no image file has been provided, assign it the default name.
+if [ "$IMAGE_FILE" = '' ]; then
+  IMAGE_FILE=$DEF_IMAGE_FILE
 fi
-
 
 # NOTE:
 # The version of Qemu (1.0), delivered with my distribution of Linux, does not emulate the
@@ -32,4 +60,4 @@ fi
 #QEMUBIN=qemu-system-arm
 QEMUBIN=~/qemu/bin/qemu-system-arm
 
-$QEMUBIN -M versatilepb -nographic -m 128 -kernel $IMAGE_FILE
+$QEMUBIN -M versatilepb -nographic -m 128 -kernel $IMAGE_FILE $QEMU_GDB_ARG
