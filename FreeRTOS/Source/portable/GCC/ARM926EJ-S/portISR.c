aa1507fa01c37bd2c66e33c4e62de235fb97ece1
Jernej Kovacic <jkovacic@gmail.com>
2020-09-21 09:21:30 +0200

    upgrade to FreeRTOS 10.4.0

--- FreeRTOS/Source/portable/GCC/ARM926EJ-S/portISR.c
+++ FreeRTOS/Source/portable/GCC/ARM926EJ-S/portISR.c
@@ -12,8 +12,8 @@
  */
 
 /*
- * FreeRTOS Kernel V10.2.1
- * Copyright (C) 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ * FreeRTOS Kernel V10.4.0
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -32,12 +32,11 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
- * http://www.FreeRTOS.org
- * http://aws.amazon.com/freertos
+ * https://www.FreeRTOS.org
+ * https://github.com/FreeRTOS
  *
  * 1 tab == 4 spaces!
  */
-
 
 /*-----------------------------------------------------------
  * Components that can be compiled to either ARM or THUMB mode are
