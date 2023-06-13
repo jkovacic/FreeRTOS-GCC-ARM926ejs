56bfa3b6e7ed7fae9f4f7c2b6cfbe0955b14e39e
Florian La Roche <Florian.LaRoche@gmail.com>
2023-06-13 20:48:19 +0200

    update to current FreeRTOS

--- FreeRTOS/portable/GCC/ARM926EJ-S/portmacro.h
+++ FreeRTOS/portable/GCC/ARM926EJ-S/portmacro.h
@@ -88,7 +88,6 @@ typedef uint32_t UBaseType_t;
 #define portBYTE_ALIGNMENT          8
 #define portNOP()                   __asm volatile ( "NOP" );
 #define portINLINE __inline
-#define portNORETURN                __attribute__((noreturn))
 #define portDONT_DISCARD            __attribute__((used))
 /*-----------------------------------------------------------*/
 
